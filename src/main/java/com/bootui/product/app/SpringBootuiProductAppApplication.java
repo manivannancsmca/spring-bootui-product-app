c20172332983fc7b9d19166644c133bb25e0adf7
manivannancsmca <manivannancsmca@gmail.com>
2026-08-16 23:32:11 +0530

    first commit

---
+++ src/main/java/com/bootui/product/app/SpringBootuiProductAppApplication.java
@@ -0,0 +1,13 @@
+package com.bootui.product.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootuiProductAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootuiProductAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootui/product/app/SpringBootuiProductAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootui.product.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootuiProductAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
