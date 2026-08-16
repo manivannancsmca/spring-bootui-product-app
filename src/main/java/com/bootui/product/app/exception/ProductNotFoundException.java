4be67d8d5268b97eba2ff5ed2d123bf0f985122f
manivannancsmca <manivannancsmca@gmail.com>
2026-08-16 23:46:42 +0530

    ProductNotFoundException custom exception class added

---
+++ src/main/java/com/bootui/product/app/exception/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.bootui.product.app.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
