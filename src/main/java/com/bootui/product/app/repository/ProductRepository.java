4d6f4f8982de710f38f8359c79a36edc6abb5f48
manivannancsmca <manivannancsmca@gmail.com>
2026-08-16 23:49:39 +0530

    ProductRepository interface added

---
+++ src/main/java/com/bootui/product/app/repository/ProductRepository.java
@@ -0,0 +1,21 @@
+package com.bootui.product.app.repository;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bootui.product.app.domain.Product;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Optional<Product> findBySku(String sku);
+
+    boolean existsBySku(String sku);
+
+    Page<Product> findByCategory(String category, Pageable pageable);
+}
